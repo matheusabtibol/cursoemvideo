49c9522ad0e1b437d6a9b325ff7fec8a6007a81b
Matheus Abtibol <matheusabtibol@gmail.com>
2022-02-18 22:23:00 -0300

    Initial commit

---
+++ 001-aula.py
@@ -0,0 +1,6 @@
+n1=int(input("digite um número"))
+print (type (n1))
+n2=int(input("digite outro valor"))
+print (type (n2))
+s=n1+n2
+print ('a soma entre {} e {} é igual a {}'.format(n1,n2, s) )
---
+++ 002-aula.py
@@ -0,0 +1,9 @@
+# Simple assignment
+from selenium.webdriver import Opera
+
+driver = Opera()
+# Or use the context manager
+from selenium.webdriver import Opera
+
+with Opera() as driver:
+# your code inside this indent
